77d1fc261821e6ba6a35b3f6bea716a466d2e4be
Robert Pliszka <robplisz@gmail.com>
2022-06-29 18:26:30 -0400

    Added User Controller that contains userController to perform API

---
+++ controllers/user-controller.js
@@ -0,0 +1,69 @@
+const { User } = require("../models");
+
+const userController = {
+  // Get all Users
+  getAllUsers(req, res) {
+    User.find({})
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+
+  // Get User by id
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate({
+        path: "thoughts",
+        select: "-__v",
+      })
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+
+  //   Create new User
+  createUser({ body }, res) {
+    User.create(body)
+      .then((dbUseData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.json(err);
+      });
+  },
+
+  //   update User by id
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No User with this ID!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  },
+  //   delete User
+  deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+      .then((dbUserData) => {
+        if (dbUserData) {
+          res.status(404).json({ message: "No User with this ID!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+};
+
+module.exports = userController;
