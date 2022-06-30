433e8f58d5e1d014f0473534417c4a6e0017e324
Robert Pliszka <robplisz@gmail.com>
2022-06-29 20:15:49 -0400

    Added though controller which has functions for API requests

---
+++ controllers/thought-controller.js
@@ -0,0 +1,57 @@
+const { model } = require("mongoose");
+const { User, Thought } = require("../models");
+
+const thoughtController = {
+  // get all thoughts
+  getAllThought(req, res) {
+    Thought.find({})
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+  //   get thought by id
+  getThoughtById({ params }, res) {
+    Thought.findOne({ _id: params.id })
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+  //   create new thought
+  createThought({ body }, res) {
+    Thought.create(body)
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => res.json(err));
+  },
+  //   update thought
+  updateThought({ params, body }, res) {
+    Thought.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .then((dbThoughData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: "No Thought with this id!" });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+  //   delete though
+  deleteThought({ params }, res) {
+    Thought.findOneAndDelete({ _id: params.id })
+      .then((dbThoughData) => {
+        if (!dbThoughData) {
+          res.status(404).json({ message: "No Thought with this id!" });
+          return;
+        }
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+};
+
+module.exports = thoughtController;
