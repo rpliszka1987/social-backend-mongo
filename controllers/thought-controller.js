d7453a58d394645276b2e20a4b180313795d345c
Robert Pliszka <robplisz@gmail.com>
2022-06-30 13:02:43 -0400

    Added reactions to the thought controller

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -52,6 +52,40 @@ const thoughtController = {
       })
       .catch((err) => res.status(400).json(err));
   },
+
+  //   add reaction
+  addReaction({ params, body }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $push: { reactions: body } },
+      { new: true }
+    )
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: "No Thought with this id!" });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+
+  //   remove reaction
+  removeReaction({ params }, res) {
+    Thought.findOneAndDelete(
+      { _id: params.thoughtId },
+      { $pull: { reactions: { reactionId: params.reactionId } } },
+      { new: true }
+    )
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: "No Thought with this id!" });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
 };
 
 module.exports = thoughtController;
